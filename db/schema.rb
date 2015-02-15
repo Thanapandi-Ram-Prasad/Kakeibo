d67b65487f8f2eb74d913523e6c0791bce7e7e2b
parasado <thanapandiramprasad@gmail.com>
2015-02-16 06:35:26 +0900

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215110141) do
+ActiveRecord::Schema.define(version: 20150215212937) do
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150215110141) do
     t.integer "user_id"
     t.text    "title"
     t.integer "price"
-    t.integer "spent_date"
+    t.text    "spent_date"
   end
 
   create_table "users", force: :cascade do |t|
