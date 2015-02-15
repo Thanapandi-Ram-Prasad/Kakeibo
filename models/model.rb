cb9605ee29552b8ef25896ea88be523749ffc364
parasado <thanapandiramprasad@gmail.com>
2015-02-16 01:36:15 +0900

    Rakefile updated

--- models/model.rb
+++ models/model.rb
@@ -1,4 +1,6 @@
-ActiveRecord::Base.establish_connection(
-  "adapter" => "sqlite3",
-  "database" => "development.sqlite3"
-)
+#ActiveRecord::Base.establish_connection(
+#  "adapter" => "sqlite3",
+#  "database" => "development.sqlite3"
+#)
+
+ActiveRecord::Base.establish_connection('sqlite3:///development.sqlite3')
