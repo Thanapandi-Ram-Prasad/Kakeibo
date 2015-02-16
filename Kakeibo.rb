3fa570c15d07a0e77ebbf33f4b69381c03dc2795
parasado <thanapandiramprasad@gmail.com>
2015-02-17 03:10:26 +0900

    initial commit

--- Kakeibo.rb
+++ Kakeibo.rb
@@ -103,7 +103,7 @@ get '/new_format' do
   end
 
   user_id = session[:user_id]
-  @lists = List.where(user_id: user_id).where("select category_id from lists group by category_id")
+  @lists = List.where(user_id: user_id).select('category_id').group('category_id')
   erb :new
 end
 
